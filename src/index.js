3babf6c60dd6fcd8c10d4437fd70e6486b8f97fb
Karjeje <193631637+Karjeje@users.noreply.github.com>
2026-01-18 02:38:03 +0100

    Create function for building the tree with recursion

--- src/index.js
+++ src/index.js
@@ -11,3 +11,18 @@ class Tree {
     this.root = buildTree(arr);
   }
 }
+
+function buildTree(array, start, end) {
+  if (array.length === 0) return null;
+  if (start > end) return null;
+  const set = [...new Set(array)];
+  set.sort((a, b) => a - b);
+
+  let mid = start + Math.floor((end - start) / 2);
+  let root = new Node(array[mid]);
+
+  root.left = buildTree(array, start, mid - 1);
+  root.right = buildTree(array, mid + 1, end);
+
+  return root;
+}
