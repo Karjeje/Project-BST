9d6c74bab210f701be6725be2b7896477a304009
Karjeje <193631637+Karjeje@users.noreply.github.com>
2026-01-18 02:40:17 +0100

    Add function for displaying tree in a structured format

--- src/index.js
+++ src/index.js
@@ -26,3 +26,16 @@ function buildTree(array, start, end) {
 
   return root;
 }
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
